0fd50b5fde7f714fcdeee7976834714f3bcf367b
hlljh5 <john158@revature.net>
2022-03-27 23:02:42 -0500

    Update ProductEntry.kt
    Added drop down lists

--- app/src/main/java/com/revature/expiration_date/ProductEntry.kt
+++ app/src/main/java/com/revature/expiration_date/ProductEntry.kt
@@ -1,29 +1,44 @@
 package com.revature.expiration_date
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import androidx.core.content.ContextCompat.startActivity
 import com.revature.expiration_date.ui.theme.Expiration_DateTheme
+import java.util.*
 
 class ProductEntry : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,84 +55,172 @@ class ProductEntry : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ProductEntryScreen() {
     val context = LocalContext.current
     val keyboardController = LocalSoftwareKeyboardController.current
+    var expanded by remember {mutableStateOf (false)}
+    val productList = listOf("Eggs", "Milk", "Lettuce", "Pork chops")
+    val categoryList = listOf("Dairy", "Vegetables", "Meat", "Fruits")
+    val locationList = listOf("fridge", "freezer", "pantry", "counter")
+    val location = rememberSaveable{mutableStateOf("")}
+    var productName by rememberSaveable {mutableStateOf("")}
+    var category = rememberSaveable {mutableStateOf("")}
+    var textFiledSize by remember { mutableStateOf(Size.Zero)}
+
+    var icon = if (expanded) {
+        Icons.Filled.KeyboardArrowUp
+    }else{
+        Icons.Filled.KeyboardArrowDown
+    }
 
     Column {
         //Top App Bar
         TopAppBar() {
             Text(text = "Product Entry")
         }
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
             //TextField Product entry
-            val productName = rememberSaveable{
-                mutableStateOf("")
-            }
-            TextField(
-                value = productName.value,
-                onValueChange = {productName.value = it},
-                label = {
-                    Text(text = "Enter Product Name")
+//            val productName = rememberSaveable{
+//                mutableStateOf("")
+
+            OutlinedTextField(
+                value = productName,
+                onValueChange = { productName = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned { coordinates ->
+                        textFiledSize = coordinates.size.toSize()
+                    },
+                    label = {Text(text = "Enter Product Name")},
+                        trailingIcon = {
+                    Icon(icon, "", Modifier.clickable { expanded = !expanded })
                 },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()})
-            )
-            //I will be trying to use a search function with the Product entry field
-            //TextField Expiration Date entry (Can we get a calendar to pop up
-            // so we can click on a date?) That is my plan.
-            val expirationDate = rememberSaveable{
-                mutableStateOf("")
-            }
-            TextField(
-                value = expirationDate.value,
-                onValueChange = {expirationDate.value = it},
-                label = {
-                    Text(text = "Enter Expiration Date")
-                },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
+                  keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                  keyboardActions = KeyboardActions(
                     onDone = {keyboardController?.hide()})
             )
 
-            //Dropdown list Category
-            val category = rememberSaveable{
-                mutableStateOf("")
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+            ) {
+                productList.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        productName = label
+                        expanded = false
+                    }) {
+                        Text(text = label)
+                    }
+                }
             }
-            TextField(
-                value = category.value,
-                onValueChange = {category.value = it},
-                label = {
-                    Text(text = "Enter Category")
-                },
+            //Expiration Date
+ /*
+            val year: Int
+            val month: Int
+            val day: Int
+
+            val calendar = Calendar.getInstance()
+            year = calendar.get(Calendar.YEAR)
+            month = calendar.get(Calendar.MONTH)
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+            calendar.time = Date()
+
+        val date = remember{mutableStateOf("")}
+        val datePickerDialog = DatePickerDialog(
+            context,
+            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                date.value = "$dayOfMonth/$month/$year"
+            }, year, month, day)
+
+            OutlinedTextField(Text(text = "Selected Date: ${date.value}")
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = {
+                    datePickerDialog.show()
+                } )  {
+                Text(text = "Enter Expiration Date")
+            },
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(
                     onDone = {keyboardController?.hide()})
             )
+*/
+            //Dropdown list Category
+
+         OutlinedTextField(
+            value = category.value,
+            onValueChange = { category.value = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFiledSize = coordinates.size.toSize()
+                },
+            label = {Text(text = "Enter Category")},
+            trailingIcon = {
+                Icon(icon, "", Modifier.clickable { expanded = !expanded })
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {keyboardController?.hide()})
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+        ) {
+            categoryList.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    category.value = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
 
             //Dropdown list Location
-            val location = rememberSaveable{
-                mutableStateOf("")
-            }
-            TextField(
+
+            OutlinedTextField(
                 value = location.value,
-                onValueChange = {location.value = it},
-                label = {
-                    Text(text = "Enter Location")
+                onValueChange = { location.value = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned { coordinates ->
+                        textFiledSize = coordinates.size.toSize()
+                    },
+                label = {Text(text = "Enter Location")},
+                trailingIcon = {
+                    Icon(icon, "", Modifier.clickable { expanded = !expanded })
                 },
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(
                     onDone = {keyboardController?.hide()})
             )
 
-            //Take a picture of the front
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+        ) {
+            locationList.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    location.value = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+
+
+
+        //Take a picture of the front
             Button(onClick = {
 
                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -151,7 +254,8 @@ fun ProductEntryScreen() {
 
         }
     }
-}
+
+
 
 @Preview(showBackground = true)
 @Composable
