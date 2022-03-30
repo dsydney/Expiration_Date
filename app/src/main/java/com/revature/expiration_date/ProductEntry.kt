1b2e9400e064b2a1802e9e6490bd4f9fec55bc0b
hlljh5 <john158@revature.net>
2022-03-29 22:30:25 -0500

    New updates
    added icons and bottom app bar

--- app/src/main/java/com/revature/expiration_date/ProductEntry.kt
+++ app/src/main/java/com/revature/expiration_date/ProductEntry.kt
@@ -10,12 +10,15 @@ import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
@@ -28,6 +31,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,15 +79,11 @@ fun ProductEntryScreen() {
     }else{
         Icons.Filled.KeyboardArrowDown
     }
-
-    Column {
-        //Top App Bar
-        TopAppBar() {
-            Text(text = "Product Entry")
-        }
-            //TextField Product entry
-//            val productName = rememberSaveable{
-//                mutableStateOf("")
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Product Entry") })
+    },
+    content = {
+        Column {
 
             OutlinedTextField(
                 value = productName,
@@ -93,13 +93,13 @@ fun ProductEntryScreen() {
                     .onGloballyPositioned { coordinates ->
                         textFiledSize = coordinates.size.toSize()
                     },
-                    label = {Text(text = "Enter Product Name")},
-                        trailingIcon = {
+                label = { Text(text = "Enter Product Name") },
+                trailingIcon = {
                     Icon(icon, "", Modifier.clickable { expanded = !expanded })
                 },
-                  keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                  keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()})
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide() })
             )
 
             DropdownMenu(
@@ -117,8 +117,8 @@ fun ProductEntryScreen() {
                     }
                 }
             }
+/*
             //Expiration Date
- /*
             val year: Int
             val month: Int
             val day: Int
@@ -150,38 +150,38 @@ fun ProductEntryScreen() {
 */
             //Dropdown list Category
 
-         OutlinedTextField(
-            value = category.value,
-            onValueChange = { category.value = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    textFiledSize = coordinates.size.toSize()
+            OutlinedTextField(
+                value = category.value,
+                onValueChange = { category.value = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned { coordinates ->
+                        textFiledSize = coordinates.size.toSize()
+                    },
+                label = { Text(text = "Enter Category") },
+                trailingIcon = {
+                    Icon(icon, "", Modifier.clickable { expanded = !expanded })
                 },
-            label = {Text(text = "Enter Category")},
-            trailingIcon = {
-                Icon(icon, "", Modifier.clickable { expanded = !expanded })
-            },
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(
-                onDone = {keyboardController?.hide()})
-        )
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide() })
+            )
 
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier
-                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
-        ) {
-            categoryList.forEach { label ->
-                DropdownMenuItem(onClick = {
-                    category.value = label
-                    expanded = false
-                }) {
-                    Text(text = label)
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+            ) {
+                categoryList.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        category.value = label
+                        expanded = false
+                    }) {
+                        Text(text = label)
+                    }
                 }
             }
-        }
 
             //Dropdown list Location
 
@@ -193,75 +193,104 @@ fun ProductEntryScreen() {
                     .onGloballyPositioned { coordinates ->
                         textFiledSize = coordinates.size.toSize()
                     },
-                label = {Text(text = "Enter Location")},
+                label = { Text(text = "Enter Location") },
                 trailingIcon = {
                     Icon(icon, "", Modifier.clickable { expanded = !expanded })
                 },
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()})
+                    onDone = { keyboardController?.hide() })
             )
 
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier
-                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
-        ) {
-            locationList.forEach { label ->
-                DropdownMenuItem(onClick = {
-                    location.value = label
-                    expanded = false
-                }) {
-                    Text(text = label)
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+            ) {
+                locationList.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        location.value = label
+                        expanded = false
+                    }) {
+                        Text(text = label)
+                    }
                 }
             }
+
+
+            //Take a picture of the front
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+
+                Button(onClick = {}) {
+                    Image(
+                        painterResource(id = R.drawable.camera_icon),
+                        contentDescription = "Camera icon",
+                        modifier = Modifier.size(20.dp)
+                    )
+
+                    Text(text = "Add a photo", Modifier.padding(start = 10.dp))
+                }
+
+                Button(onClick = {}) {
+                    Image(
+                        painterResource(id = R.drawable.image_icon),
+                        contentDescription = "Image icon",
+                        modifier = Modifier.size(20.dp)
+                    )
+
+                    Text(text = "Add an image", Modifier.padding(start = 10.dp))
+                }
+
+
+            }
+
         }
 
+    },
+        bottomBar = {BottomBar()}
+        )
+}
+
+@Composable
+fun BottomBar() {
+
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation(elevation = 10.dp) {
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Delete, "")
+        },
+            label = { Text(text = "Cancel") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Check, "")
+        },
+            label = { Text(text = "OK") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            })
 
 
-        //Take a picture of the front
-            Button(onClick = {
-
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                context.startActivity(intent)
-
-            }) {
-
-                Text(text = "Take Picture of Item")
-
-            }
-            //Take a picture of the expiration date
-            Button(onClick = {
-
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                context.startActivity(intent)
-
-            }) {
-
-                Text(text = "Take Picture of Expiration Date")
-
-            }
-
-            Button(onClick = {
-                //Save new item to ... a database? An arrayList? An arraylist of Items, where Items
-                // is a class with parameters for name, location, category, etc could work
-            }) {
-                Text(text = "Save")
-            }
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-        }
     }
 
 
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview4() {
     Expiration_DateTheme {
-        ProductEntryScreen()
+
     }
 }
 
