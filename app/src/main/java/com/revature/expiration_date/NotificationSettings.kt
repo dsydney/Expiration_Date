01abb60dc47eb8cbb9810355b0aa687b1a77ce36
DietLasagna <100707359+DietLasagna@users.noreply.github.com>
2022-03-29 19:19:11 -0500

    Updated UI for NotificationSettings.kt

--- app/src/main/java/com/revature/expiration_date/NotificationSettings.kt
+++ app/src/main/java/com/revature/expiration_date/NotificationSettings.kt
@@ -1,37 +1,35 @@
 package com.revature.expiration_date
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.revature.expiration_date.ui.theme.Expiration_DateTheme
 
-//class NotificationSettings : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            Expiration_DateTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    NotificationSettingsScreen()
-//                }
-//            }
-//        }
-//    }
-//}
-
+/*
+Notifications can trigger for 3 days prior, 1 day prior, day of expiration, and/or expired.
+ */
 @Composable
 fun NotificationSettingsScreen() {
-    // ADD state variables for each setting
+    var selectThreeDays: Boolean by rememberSaveable { mutableStateOf(false) }
+    var selectOneDay: Boolean by rememberSaveable { mutableStateOf(false) }
+    var selectDayOf: Boolean by rememberSaveable { mutableStateOf(true) }
+    var selectAfter: Boolean by rememberSaveable { mutableStateOf(false) }
+
+    val switchColorsSet = SwitchDefaults.colors(
+        checkedThumbColor = MaterialTheme.colors.primary,
+        uncheckedThumbColor = MaterialTheme.colors.primary,
+        checkedTrackColor = MaterialTheme.colors.primaryVariant,
+        uncheckedTrackColor = MaterialTheme.colors.secondaryVariant
+    )
+    val rowModifier = Modifier.fillMaxWidth(0.75f).padding(16.dp)
 
     Scaffold(
         topBar = {
@@ -44,26 +42,80 @@ fun NotificationSettingsScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                //List of all notifications
-                //Settings to change notifications
-                //   Who gets them (Phone numbers, emails), When (1 day, 3 days, past expiry, etc)
-
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth(0.85f)
+                        .fillMaxWidth(0.9f)
                         .padding(16.dp),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Remind me:",
+                        style = MaterialTheme.typography.h5
+                    )
+                }
+                Row(
+                    modifier = rowModifier,
                     horizontalArrangement = Arrangement.End,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = "Setting 1",
-                        style = MaterialTheme.typography.h5
+                        text = "Three days before",
+                        style = MaterialTheme.typography.h6
                     )
                     Spacer(Modifier.width(24.dp))
                     Switch(
-                        checked = false,
-                        onCheckedChange = {  },
-//                        colors = switchColorsSet
+                        checked = selectThreeDays,
+                        onCheckedChange = { selectThreeDays = it },
+                        colors = switchColorsSet
+                    )
+                }
+                Row(
+                    modifier = rowModifier,
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "One day before",
+                        style = MaterialTheme.typography.h6
+                    )
+                    Spacer(Modifier.width(24.dp))
+                    Switch(
+                        checked = selectOneDay,
+                        onCheckedChange = { selectOneDay = it },
+                        colors = switchColorsSet
+                    )
+                }
+                Row(
+                    modifier = rowModifier,
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "On expiration date",
+                        style = MaterialTheme.typography.h6
+                    )
+                    Spacer(Modifier.width(24.dp))
+                    Switch(
+                        checked = selectDayOf,
+                        onCheckedChange = { selectDayOf = it },
+                        colors = switchColorsSet
+                    )
+                }
+                Row(
+                    modifier = rowModifier,
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "After expiration date",
+                        style = MaterialTheme.typography.h6
+                    )
+                    Spacer(Modifier.width(24.dp))
+                    Switch(
+                        checked = selectAfter,
+                        onCheckedChange = { selectAfter = it },
+                        colors = switchColorsSet
                     )
                 }
             }
