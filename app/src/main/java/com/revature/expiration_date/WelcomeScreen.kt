2de0ef2d812f362c6144dd1df7da6f27aea54751
dsydney <100386938+dsydney@users.noreply.github.com>
2022-04-01 17:58:17 -0500

    Added a bare splash screen. Still need to add an image or title

--- app/src/main/java/com/revature/expiration_date/WelcomeScreen.kt
+++ app/src/main/java/com/revature/expiration_date/WelcomeScreen.kt
@@ -8,11 +8,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.revature.expiration_date.ui.theme.Expiration_DateTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +30,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Main()
+                    Navigation()
                 }
             }
         }
     }
+}
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+        composable("main_screen") {
+            Main()
+        }
+
+    }
+
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(key1 = true) {
+        delay(5000L)
+        navController.navigate("main_screen")
+    }
+    Text(text = "Splash")
 }
 
 @Composable
